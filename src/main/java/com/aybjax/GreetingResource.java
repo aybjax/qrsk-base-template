bb5da6d0568875ab3b9ff1ca6646f997955f8d46
aybjax <aybat93@gmail.com>
2024-11-03 00:14:02 +0500

    Add jspecify

--- src/main/java/com/aybjax/GreetingResource.java
+++ src/main/java/com/aybjax/GreetingResource.java
@@ -5,12 +5,19 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        invalidFunction("notNull");
+        invalidFunction(null);
         return "Hello from Quarkus REST";
+    }
+
+    private void invalidFunction(String nonNullableString) {
+        System.out.println(nonNullableString);
     }
 }
---
+++ src/main/java/com/aybjax/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.aybjax;
+
+import org.jspecify.annotations.NullMarked;
